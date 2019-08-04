c55c376d97722f58a0d015ef871d755eb08416c1
Happytreat <cowfats@gmail.com>
2019-08-04 14:30:17 +0800

    Add aws amplify config to allow UI to talk to aws services

---
+++ uplan-app-client/src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "ap-southeast-1",
+    BUCKET: "uplan-app-uploads"
+  },
+  apiGateway: {
+    REGION: "ap-southeast-1",
+    URL: "https://nrg11gqaz9.execute-api.ap-southeast-1.amazonaws.com/dev"
+  },
+  cognito: {
+    REGION: "ap-southeast-1",
+    USER_POOL_ID: "ap-southeast-1_s6AfaLltO",
+    APP_CLIENT_ID: "467trou0fbnm1l9c19akv9b80",
+    IDENTITY_POOL_ID: "ap-southeast-1:7ced466c-e448-416d-a8e0-a6e7bde6efa4"
+  }
+};
