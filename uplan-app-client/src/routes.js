51ad0b5695c06f06c89d041f19c755c4fb31e3eb
Happytreat <cowfats@gmail.com>
2019-08-04 14:58:45 +0800

    Add a login page

--- uplan-app-client/src/routes.js
+++ uplan-app-client/src/routes.js
@@ -2,9 +2,11 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "./containers/pages/HomePage/homepage";
 import NotFound from "./containers/pages/404";
+import Login from "./containers/LoginForm/LoginForm";
 
 export default () =>
   <Switch>
     <Route path="/" exact component={HomePage} />
+    <Route path="/login" exact component={Login} />
     <Route component={NotFound} />
   </Switch>;
