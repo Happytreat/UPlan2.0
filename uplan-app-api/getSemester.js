f6c9d3f1ca9e75de8202e4648dfed1705b76a093
Happytreat <cowfats@gmail.com>
2019-08-06 10:36:27 +0800

    Use env variables for tableNames

--- uplan-app-api/getSemester.js
+++ uplan-app-api/getSemester.js
@@ -1,9 +1,10 @@
 import * as dynamoDbLib from "./libs/dynamodb-lib";
 import { success, failure } from "./libs/response-lib";
+import { SemestersTable } from "./consts/tables";
 
 export async function main(event, context) {
   const params = {
-    TableName: "semesters",
+    TableName: SemestersTable,
     Key: {
       userId: event.requestContext.identity.cognitoIdentityId,
       semesterId: event.pathParameters.id
