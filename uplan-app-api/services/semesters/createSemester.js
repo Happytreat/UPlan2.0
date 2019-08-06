aea27d35e476dbb7754ea633d469f854124b72c9
Happytreat <cowfats@gmail.com>
2019-08-06 12:39:09 +0800

    Organize Lambda functions to services dir

--- uplan-app-api/services/semesters/createSemester.js
+++ uplan-app-api/services/semesters/createSemester.js
@@ -1,7 +1,7 @@
 import uuid from "uuid";
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
-import { SemestersTable } from "./consts/tables";
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+import { SemestersTable } from "../../consts/tables";
 
 export async function main(event, context) {
   const data = JSON.parse(event.body);
--- uplan-app-api/services/semesters/deleteSemester.js
+++ uplan-app-api/services/semesters/deleteSemester.js
@@ -1,6 +1,6 @@
-import * as dynamoDbLib from "./libs/dynamodb-lib";
-import { success, failure } from "./libs/response-lib";
-import { SemestersTable } from "./consts/tables";
+import * as dynamoDbLib from "../../libs/dynamodb-lib";
+import { success, failure } from "../../libs/response-lib";
+import { SemestersTable } from "../../consts/tables";
 
 export async function main(event, context) {
   const params = {
