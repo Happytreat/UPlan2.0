3e690246fd21b330c3d922f68c7a835ce43f86b5
Happytreat <cowfats@gmail.com>
2019-08-12 23:42:43 +0800

    refactor: 💡 getModuleList returns modules for array of sems
    Return modules in dnd required format

--- uplan-app-api/services/modules/getModuleList.js
+++ uplan-app-api/services/modules/getModuleList.js
@@ -1,28 +1,36 @@
-import { forEach } from 'lodash';
+// import { forEach } from 'lodash';
 import * as dynamoDbLib from "../../libs/dynamodb-lib";
 import { success, failure } from "../../libs/response-lib";
 import { ModulesTable } from "../../consts/tables";
 
-// @return all modules of a given semester and total credits of the modules
+// @return all modules of a list of semester ids
 export async function main(event, context) {
-  const params = {
-    TableName: ModulesTable,
-    KeyConditionExpression: "semesterId = :semesterId",
-    ExpressionAttributeValues: {
-      ":semesterId": event.body.semesterId
-    }
-  };
-
+  /**
+   * Should look like:
+   * modules: {
+   *   "semId": [{moduleId: ...}, {moduleId: ...}],
+   *   "semId": [{moduleId: ...}, {moduleId: ...}],
+   * }
+   * */
   try {
-    const result = await dynamoDbLib.call("query", params);
-    let totalCredits = 0;
-    forEach(result, mod => { totalCredits += mod.credits; });
+    let modules = {};
+    await Promise.all(event.body.semesters.map(async (semId) => {
+      const params = {
+        TableName: ModulesTable,
+        KeyConditionExpression: "semesterId = :semesterId",
+        ExpressionAttributeValues: {
+          ":semesterId": semId,
+        }
+      };
+
+      const { Items: moduleList } = await dynamoDbLib.call("query", params);
+      modules[semId] = moduleList;
+    }));
 
     return success({
-      modules: result.Items,
-      totalCredits,
+      modules,
     });
   } catch (e) {
-    return failure({ status: false });
+    return failure({ status: false, error: e });
   }
 }
