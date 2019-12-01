808228ee9584608698edb0a1f21cd53c3a3e6bae
Happytreat <cowfats@gmail.com>
2019-12-01 20:09:24 +0800

    docs: ✏️ Update Version and App name from webpack

---
+++ uplan-app-client/src/utils/logger.js
@@ -0,0 +1,39 @@
+
+/*
+This file is done to confuse the automatic removal of console log statements in the webpack build,
+and to allow for future functionality to send logs somewhere on errors
+ */
+const logger = console;
+
+const trace = (...args) => {
+  logger.trace(...args);
+};
+
+const debug = (...args) => {
+  logger.debug(...args);
+};
+
+const info = (...args) => {
+  logger.info(...args);
+};
+
+const warn = (...args) => {
+  logger.warn(...args);
+};
+
+const error = (...args) => {
+  logger.error(...args);
+};
+
+const fatal = (...args) => {
+  logger.fatal(...args);
+};
+
+export default {
+  trace,
+  debug,
+  info,
+  warn,
+  error,
+  fatal,
+};
