df7e7a6a533c6e2a496fc72e9b061f72e3275f9a
Happytreat <cowfats@gmail.com>
2019-12-04 16:38:44 +0800

    style: 💄 Styled the NotLoggedIn Page with theme colors

--- uplan-app-client/src/organisms/pages/HomePage/NotLoggedIn.js
+++ uplan-app-client/src/organisms/pages/HomePage/NotLoggedIn.js
@@ -1,29 +1,40 @@
 import React, { Component } from "react";
-import PlanImg from '../../../asset/plan.svg'
+import styled from 'styled-components'
 
-const styles = {
-  main: {
-    padding: '4rem 0',
-    textAlign: 'center',
-  },
-  p: {
-    color: '#999',
-  },
-  h1: {
-    fontFamily: "Open Sans sans-serif",
-    fontWeight: '600',
-  },
-};
+import Logo from "../../../asset/unicorn-icon-transparent.png";
+import PlanImg from '../../../asset/undraw_schedule_pnbk.svg'
 
+const MainLogo = styled.img`
+   border: 1px solid ${props => props.theme.secondary};
+   border-radius: 100px;
+   margin-bottom: 10px;
+`;
+
+const MainDiv = styled.div`
+  padding: 4rem 0;
+  text-align: center;
+`;
+
+const Description = styled.p`
+  color: ${props => props.theme.secondary};
+`;
+
+const AppPreview = styled.img`
+   max-width: 60vh;
+   padding: 0 1rem 0 0;
+`;
+
+// TODO: Replace with App Preview Image
 export default class NotLoggedIn extends Component {
   render() {
     return (
-      <div style={styles.main}>
-        <h1 style={styles.h1}>UPlan</h1>
-        <p style={styles.p}>A visual university planning app</p>
+      <MainDiv>
+        <MainLogo src={Logo} alt={Logo} />
+        <h4>Visual university planner with the student in mind</h4>
+        <Description>UPlan, we do the rest.</Description>
         <br />
-        <img src={PlanImg} alt="HomePage" style={{ maxWidth: '60vh', padding: '0 1rem 0 0' }} />
-      </div>
+        <AppPreview src={PlanImg} alt="AppPreview" />
+      </MainDiv>
     );
   }
 }
