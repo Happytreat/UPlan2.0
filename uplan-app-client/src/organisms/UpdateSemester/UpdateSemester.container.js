1fff4d891bb71b102619732f5a7ace0b85c68abb
Happytreat <cowfats@gmail.com>
2019-12-10 22:13:47 +0800

    refactor: 💡 Refactor DeleteSemester on Frontend

--- uplan-app-client/src/organisms/UpdateSemester/UpdateSemester.container.js
+++ uplan-app-client/src/organisms/UpdateSemester/UpdateSemester.container.js
@@ -2,7 +2,6 @@ import { connect } from "react-redux";
 import { API } from "aws-amplify";
 import UpdateSemester from './UpdateSemester.component';
 import { actions as userActions, selectors as user } from '../../store/user/user.ducks';
-import {get} from "lodash";
 
 function mapStateToProps(state) {
   return {
@@ -20,26 +19,10 @@ function mapDispatchToProps(dispatch) {
       const semesters = await API.get("api", "/semesters"); // TODO: put this in saga
       dispatch(userActions.update({ semesters }));
     },
-    deleteSemester: async (id, moduleList) => {
+    deleteSemester: async (id) => {
       try {
-        // TODO: To remove Hack for fast visual update
-        dispatch(userActions.alt());
-
-        // Delete all modules in that semester
-        // TODO: Consider moving this to backend - delete all mod of sem
-        // Since backend will have most updated module
-        // await Promise.all(get(moduleList, `${id}`, []).map(async (mod) => {
-        //   return API.del("api", `/delete-module/${mod.moduleId}`);
-        // }));
-        //
-        // // TODO: Replan backend for this
-        // // Update store's modules
-        // const { modules } = await API.get("api", "/get-modules-list");
-
         await API.del("api", `/semesters/${id}`);
         const semesters = await API.get("api", "/semesters");
-
-        // dispatch(userActions.update({ modules }));
         dispatch(userActions.update({ semesters }));
       } catch (err) {
         console.log('delete err', err);
