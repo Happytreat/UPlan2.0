758404fa250e61e79fed8362903e2c78c4f86485
Happytreat <cowfats@gmail.com>
2019-12-11 12:24:31 +0800

    test: 💍 Add getModuleTest

--- uplan-app-api/tests/unit-tests/get-module.test.js
+++ uplan-app-api/tests/unit-tests/get-module.test.js
@@ -1,7 +1,7 @@
 import Modules from "../../services/modules/modules";
 import testDb from './mock';
 
-it('works with resolves', async () => {
+it('Get Module Successfully if Exists', async () => {
   expect.assertions(1);
   const moduleTable = new Modules(testDb, "moduleTable");
   const moduleExist = 'mod-1';
@@ -10,5 +10,17 @@ it('works with resolves', async () => {
   expect(data).toEqual({
     moduleId: 'mod-1',
     userId: 'user-1'
+  });
+});
+
+it('getModule return failure if module does not exists', async () => {
+  expect.assertions(1);
+  const moduleTable = new Modules(testDb, "moduleTable");
+  const moduleNotExist = 'mod-2';
+  const userNotExist = 'user-2';
+  const data = await moduleTable.get(userNotExist, moduleNotExist, (body) => body, (body) => body);
+  expect(data).toEqual({
+    error: 'Item not found.',
+    status: false
   });
 });
